72638e508201ef9b6028b89cc0c7d5cb18ea50ec
danielhgm <competitivogral756@gmail.com>
2024-07-10 22:06:02 -0600

    Cambios en html css y js --> en el js tengo que agregar algo que quite

--- static/scripts/scripts2.js
+++ static/scripts/scripts2.js
@@ -1,3 +1,5 @@
+//abrir y cerrar menu lateral
+
 function openNav() {
     var links = document.getElementById("mySidenav").querySelectorAll("a");
     links.forEach(function(link) {
@@ -16,22 +18,5 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-function toggleContrast() {
-    var elementsToToggle = document.querySelectorAll('body, .right-side'); // Añadir aquí otros selectores si es necesario
-    elementsToToggle.forEach(function(element) {
-        if (element.classList.contains('dark-mode')) {
-            element.classList.remove('dark-mode');
-            element.classList.add('light-mode');
-        } else {
-            element.classList.remove('light-mode');
-            element.classList.add('dark-mode');
-        }
-    });
-}
 
 
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
