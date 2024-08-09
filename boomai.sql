3079206f5c88d12b5f242d72e411ab78f493b153
danielhgm <competitivogral756@gmail.com>
2024-08-08 20:47:26 -0600

    Elimine BOOMAI.sql , dump2024.sql y agregue la nueva bd funcional boomai.sql, actualize librerias de PIP para que las actualizes ivan por si falta algo

--- boomai.sql
+++ boomai.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `boomai` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `boomai`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
--- Host: localhost    Database: univai
+-- Host: 172.27.206.204    Database: boomai
 -- ------------------------------------------------------
--- Server version	8.0.37
+-- Server version	8.0.39-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -51,8 +53,8 @@ CREATE TABLE `facturas` (
   `id` int NOT NULL,
   `fecha` date DEFAULT NULL,
   `total` decimal(10,2) DEFAULT NULL,
-  `usuario_id` int DEFAULT NULL,
-  `empresa_id` int DEFAULT NULL,
+  `usuario_id` int NOT NULL AUTO_INCREMENT,
+  `empresa_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `usuario_id` (`usuario_id`),
   KEY `empresa_id` (`empresa_id`),
@@ -96,6 +98,33 @@ LOCK TABLES `inventario` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `producto_id` varchar(255) NOT NULL,
+  `proveedor_id` varchar(255) NOT NULL,
+  `cantidad` int NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'21','21',21,'2024-08-07');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `trabajadores`
 --
 
@@ -121,6 +150,33 @@ LOCK TABLES `trabajadores` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `contraseña` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'daniel','competitivogral756@gmail.com','$2b$12$HGiEesaDmLyyALSTJkNal.BtV1srrNWUSDP2B9yJHkcQFS00xvBsG'),(2,'daniel1','competitivogral7561@gmail.com','$2b$12$eQaq5m1f1zubNe3uHtJ96.UwF1Iuf94/ilf1betdeH3kMbMLIE5RK');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -129,12 +185,12 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `id` int NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `contraseña` varchar(255) NOT NULL,
+  `usuario` varchar(255) CHARACTER SET utf8mb3 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `contraseña` varchar(255) CHARACTER SET utf8mb3 NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,4 +211,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-06 11:43:35
+-- Dump completed on 2024-08-08 20:43:47
