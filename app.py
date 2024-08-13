8402b1a32b6dfdd3f971fc799d3e5ac52e1a9500
danielhgm <competitivogral756@gmail.com>
2024-08-13 01:28:46 -0600

    menu login ya redirecciona a google solo falta hacer debugging

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, render_template, request, jsonify, url_for, Response, Blueprint
+from flask import Flask, render_template, request, jsonify, url_for, Response, Blueprint, session, redirect
 import mysql.connector
 from mysql.connector import Error
 import joblib
@@ -11,16 +11,47 @@ from facturas.registrar_facturas import facturas_bp
 from facturas.registrar_proveedores import proveedores_bp
 from facturas.registrar_gastos import gastos_bp
 from flask_bcrypt import Bcrypt
-
+#importacion de librerias de google
+import google.auth.transport.requests
+import google.oauth2.id_token
+#from flask_oauthlib.client import OAuth --> Este quedo con # ya que era una funcion con problema en otras versiones con otras librerias
+#(eliminar esta libreria flask_oauthlib")
+from authlib.integrations.flask_client import OAuth #este es el perron
+import os
 
 app = Flask(__name__, template_folder="templates")
 #codigo para contraseña
 app.config['SECRET_KEY'] = '1234'
 bcrypt = Bcrypt(app)
+#codigo para tokens relacionados con GOOGLE CLOUD SERVICES para inicio o creacion de cuenta con google
+#(como es servidor prueba solo pueden crearse cuenta o acceder degovan...@gmail.com y competitivogral756@gmail.com)
+#Una vez ya teniendo el dominio real ya podran acceder hasta 10k usuarios con google
+
+#este tipo de login espero que tambien funcione contigo ivan
+app.secret_key = '1234'  
+app.config['GOOGLE_ID'] = ''
+app.config['GOOGLE_SECRET'] = ''
 
 app.register_blueprint(facturas_bp, url_prefix='/facturas')
 app.register_blueprint(proveedores_bp, url_prefix='/proveedores')
 app.register_blueprint(gastos_bp, url_prefix='/gastos')
+
+# Configura OAuth para la verificacion de google
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='818050231644-68l5cd6c0hvg9ihh947guetml63381nr.apps.googleusercontent.com',
+    client_secret='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',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='/auth/callback',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
 
 
 #conexion a bd
@@ -28,17 +59,17 @@ def db_connect():
     try:
         connection = mysql.connector.connect(
             #bd dani
-            #user='root',
-            #password='Altima_2800',
-            #host='localhost',
-            #database='boomai',
-            #port='3307'
-            #bd ivan
-            user='virtualbox1',
+            user='root',
             password='Altima_2800',
-            host='172.29.193.211',
+            host='localhost',
             database='boomai',
-            port='3306'
+            port='3307'
+            #bd ivan
+            #user='virtualbox1',
+            #password='Altima_2800',
+            #host='172.29.193.211',
+            #database='boomai',
+            #port='3306'
         )
         if connection.is_connected():
             print("Conexión exitosa a la base de datos")
@@ -106,9 +137,8 @@ def register():
 
 ##Ruta para iniciar sesion
 
-
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/loginlocal', methods=['POST'])
+def loginlocal():
     if not request.is_json:
         return jsonify({"message": "Content-Type debe ser 'application/json'", "success": False}), 200
 
@@ -139,6 +169,25 @@ def login():
         if connection.is_connected():
             cursor.close()
             connection.close()
+
+# Ruta para manejar el login con Google
+    
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorized', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/auth/callback')
+def authorized():
+    token = google.authorize_access_token()
+    user_info = google.parse_id_token(token)
+    session['user_info'] = user_info
+    return 'Logged in as: ' + user_info['email']
+
+@app.route('/logout')
+def logout():
+    session.pop('user_info', None)
+    return redirect('/')
 
 if __name__ == '__main__':
    app.run(debug=True)
